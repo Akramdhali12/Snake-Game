96bce08339148a5fc463a0998ae4f26f0fb6aaa6
Akram Hossain <dhaliakram6@gmail.com>
2024-05-25 01:27:57 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+//Game Constants & Variables
+let direction = {x:0,y:0};
+const foodSound = new Audio('audio file name');
+const gameOverSound = new Audio('audio file name');
+const moveSound = new Audio('audio file name');
+const musicSound = new Audio('audio file name');
+
+
+
+
+// Game Function
+function main(ctime){
+    window.requestAnimationFrame(main);
+}
+
+
+
+
+
+
+
+//main logic starts here
+window.requestAnimationFrame(main);
