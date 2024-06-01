e8c8e7fa29119a4d7a9d19a05ba6f1be4e13ae6b
Akram Hossain <dhaliakram6@gmail.com>
2024-06-01 15:36:55 +0800

    update index.js file 2nd time

--- js/index.js
+++ js/index.js
@@ -1,29 +1,56 @@
 //Game Constants & Variables
-let direction = {x:0,y:0};
+let inputDir = {x:0,y:0};
 const foodSound = new Audio('audio file name');
 const gameOverSound = new Audio('audio file name');
 const moveSound = new Audio('audio file name');
 const musicSound = new Audio('audio file name');
-let speed = 2;
+let speed = 5;
+let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
     {x:13,y:15}
 ]
-food = {x:13,y:15};
+food = {x:6,y:7};
 
 
 // Game Function
 function main(ctime){
     window.requestAnimationFrame(main);
-    console.log(ctime);
+    // console.log(ctime);
     if((ctime - lastPaintTime)/1000 < 1/speed){
         return;
     }
     lastPaintTime = ctime;
     gameEngine();
 }
+function isCollide(sarr){
+    return false;
+}
 function gameEngine(){
     //part 1: Updating the snake array & Food
+    if(isCollide(snakeArr)){
+        gameOverSound.play();
+        musicSound.pause();
+        inputDir = {x:0,y:0};
+        alert("Game over. Press any key to play again!");
+        snakeArr = [{x: 13, y: 15}];
+        musicSound.play();
+        score = 0;
+    }
+    //If you have eaten the food, increment the score and regenerate the food
+    if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
+        foodSound.play();
+        snakeArr.unshift({x: snakeArr[0].x + inputDir.x , y:snakeArr[0].y + inputDir.y});
+        let a=2;
+        let b=16;
+        food ={x: Math.round(a+(b-a)* Math.random()), y: Math.round(a+(b-a)* Math.random())}
+    }
+    //moving the snake
+    for (let i = snakeArr.length-2; i >=0; i--){
+        snakeArr[i+1] = {...snakeArr[i]};
+    }
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y;
 
     //part 2: Render the snake and Food
     //display the snake
@@ -32,9 +59,10 @@ function gameEngine(){
         snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
-        snakeElement.classList.add('snake');
         if(index === 0){
             snakeElement.classList.add('head');
+        }else{
+            snakeElement.classList.add('snake');
         }
         board.appendChild(snakeElement);
     })
@@ -47,10 +75,35 @@ function gameEngine(){
 }
 
 
-
-
-
-
-
 //main logic starts here
 window.requestAnimationFrame(main);
+
+window.addEventListener('keydown',e=>{
+    inputDir = {x:0,y:1};//start the game
+    moveSound.play();//sound play 
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("ArrowUp");
+            inputDir.x = 0;
+            inputDir.y = -1;
+            break;
+        case "ArrowDown":
+            console.log("ArrowDown");
+            inputDir.x = 0;
+            inputDir.y = 1;
+            break;
+        case "ArrowLeft":
+            console.log("ArrowLeft");
+            inputDir.x = -1;
+            inputDir.y = 0;
+            break;
+        case "ArrowRight":
+            console.log("ArrowRight");
+            inputDir.x = 1;
+            inputDir.y = 0;
+            break;
+    
+        default:
+            break;
+    }
+})
